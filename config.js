eb65f4e6f936548f39e280c4d073496b8ec655b1
shouko <imshouko@gmail.com>
2021-03-22 23:58:44 +0900

    Fix config

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   port: process.env.PORT || 3000,
   upstream: process.env.UPSTREAM_URL || 'http://example.com/live',
   basePath: process.env.BASE_PATH || '/foo_bar',
-  useMorgan: Boolean(process.env.USE_MORGAN) || false,
+  useMorgan: parseInt(process.env.USE_MORGAN) || false,
 }
